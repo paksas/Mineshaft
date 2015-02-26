fe8b033e61a83e7f2e5eaa84a574e30be64731ef
azagor <szczepankowski@gmail.com>
2015-02-26 22:51:15 +0100

    test
    test

---
+++ Project/Assets/Scripts/Utils/AdmobManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdmobManager : MonoBehaviour
+{
+    private AdMobPlugin m_admob;
+
+
+    void Awake  ()
+    {
+        m_admob = GetComponent<AdMobPlugin>();
+    }
+    // Use this for initialization
+    void Start()
+    {
+        m_admob.CreateBanner("", AdMobPlugin.AdSize.BANNER, true, "", true);
+        m_admob.RequestAd();
+        m_admob.ShowBanner();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
